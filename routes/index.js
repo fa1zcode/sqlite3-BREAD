7bf2966907594f210f0961be2ef3f0840a1be279
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-03-05 22:27:58 +0700

    add and delete features

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,46 @@ router.get("/", function (req, res) {
   });
 });
 
+router.get("/add", function (req, res) {
+  res.render("add");
+});
+
+router.post("/add", function (req, res) {
+  
+  // query binding = use (?) to prevent hack via sql injection
+  db.run(
+    `insert into todo (string, integer, float, date, boolean) values (?,?,?,?,?)`,
+    [
+      req.body.string,
+      parseInt(req.body.integer),
+      parseFloat(req.body.float),
+      req.body.date,
+      req.body.boolean,
+    ],
+    (err, raws) => {
+      if (err) return res.send(err);
+      console.log(req,res)
+      res.redirect("/");
+    }
+  );
+});
+
+router.get("/delete/:id", function (req, res) {
+  const id = Number(req.params.id);
+  db.run("delete from todo where id = ? ", [id], (err, raws) => {
+    if (err) return res.send(err);
+    console.log(raws);
+    res.redirect("/");
+  });
+});
+
+router.get("/edit/:id", function (req, res) {
+  const id = Number(req.params.id);
+  db.get("select * from todo where id = ?", [id], (err, raws) => {
+    console.log(err);
+    if (err) return res.send(err);
+    res.render("edit", { data: raws });
+  });
+});
+
 module.exports = router;
