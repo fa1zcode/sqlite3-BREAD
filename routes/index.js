61fbc32af41e9076ad210b610e5e349898256048
faiz alwan rickwanto <nrickwanto@gmail.com>
2022-03-07 11:47:15 +0700

    search done except date

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,55 @@ var db = new sqlite3.Database(path.join(__dirname, "..", "db", "todo.db"));
 
 router.get("/", function (req, res) {
   const url = req.url == "/" ? "/?page=1" : req.url;
+
+  let params = [];
+
+  if (req.query.string) {
+    params.push(`string like '%${req.query.string}%'`);
+  }
+  if (req.query.integer) {
+    params.push(`integer = ${req.query.integer}`);
+  }
+  if (req.query.float) {
+    params.push(`float like '${req.query.float}' `);
+  }
+  if (req.query.date) {
+    params.push(`date = ${req.query.date}`);
+  }
+  if (req.query.boolean) {
+    params.push(`boolean = ${req.query.boolean}`);
+  }
+
   const page = req.query.page || 1;
   const limit = 3;
   const offset = (page - 1) * limit;
 
-  db.get(`select count (*) as total from todo`, (err, raw) => {
+  let sql = "select count(*) as total from todo";
+  if (params.length > 0) {
+    sql += ` where ${params.join(" and ")}`;
+  }
+
+  db.get(sql, (err, raw) => {
     const jumlahHalaman = Math.ceil(raw.total / limit);
-    console.log(raw);
-    db.all(
-      `select * from todo limit ? offset ?`,
-      [limit, offset],
-      (err, raws) => {
-        if (err) return res.send(err);
-        res.render("list", { data: raws, page, jumlahHalaman, url });
-      }
-    );
+    sql = `select * from todo`;
+    if (params.length > 0) {
+      sql += ` where ${params.join(" and ")}`;
+    }
+    sql += ` limit ? offset ?`;
+
+    console.log(sql)
+
+    db.all(sql, [limit, offset], (err, raws) => {
+      console.log(raws)
+      if (err) return res.send(err);
+      res.render("list", {
+        data: raws,
+        page,
+        jumlahHalaman,
+        query: req.query,
+        url,
+      });
+    });
   });
 });
 
@@ -38,7 +72,7 @@ router.post("/add", function (req, res) {
       parseInt(req.body.integer),
       parseFloat(req.body.float),
       req.body.date,
-      req.body.boolean,
+      JSON.parse(req.body.boolean),
     ],
     (err, raws) => {
       if (err) return res.send(err);
@@ -72,8 +106,8 @@ router.post("/edit/:id", function (req, res) {
     "update todo set string = ?, integer = ?, float = ?, date = ?, boolean = ? where id = ?",
     [
       req.body.string,
-      req.body.integer,
-      req.body.float,
+      parseInt(req.body.integer),
+      parseFloat(req.body.float),
       req.body.date,
       JSON.parse(req.body.boolean),
       id,
